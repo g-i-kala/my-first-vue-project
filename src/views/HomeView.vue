c09b5d1059cdba057f8472b361068b0de1c806fa
g-i-kala <5star@wp.pl>
2025-07-04 17:47:13 +0200

    refactor: play with composition api in micro scale

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup>
+import { ref } from 'vue'
 import TheWelcome from '../components/TheWelcome.vue'
+
+const message = ref("Hello bijacz!");
+
+let doSomething = () => {
+  alert("doing it yess!");
+}
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>
+      <button @click="doSomething">Click Me!</button>
+    </p>
+    <p>{{ message }}</p>
+    <input type="text" v-model="message" />
+
   </main>
 </template>
